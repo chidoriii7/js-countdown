d0a6a668c86294c08b77314bd61c6dd29bb1ba17
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-13 22:07:39 +0200

    js-countdown : completato + altri bonus (tasto reset e style pagina)

--- app.js
+++ app.js
@@ -5,39 +5,129 @@
 // Bonus 2
 // Aggiungete un bottone che interrompe il count down e mostra il messaggio;
 
-const start = document.getElementById('start')
-const stopbutton = document.getElementById('stop')
+
+// <!-- variabili -->
+
+
+const start = document.getElementById('start');
+const stopbutton = document.getElementById('stop');
+const restart = document.getElementById('restart');
+const body = document.getElementById('body');
 
 let countdown = document.getElementById('countdown');
 
-let count = 10
+let count = 10;
 let myInterval;
 
-start.addEventListener('click', function() {
+// <!-- EventListeners -->
 
+
+//start button
+start.addEventListener('click', function() {
+// <!-- Funzione Intervallo -->
     myInterval = setInterval(function(){
-        countdown.innerText = count
+
+        countdown.innerText = count;
         count--;
+
+// <!-- Controllo del contatore -->
 
         if(count < 0) {
 
             clearInterval(myInterval);
-            countdown.innerText = 'buon anno'
+            
+            countdown.innerText = 'Buon Anno!';
+
+            // <!-- add/remove classes (colore) -->
+            body.classList.toggle('default-body');
+            body.classList.add('successful');
+
+            // <!-- add/remove classes (bottoni) -->
+            start.classList.add('hidden');
+            stopbutton.classList.add('hidden');
+            restart.classList.toggle('hidden');
+
         }
 
     }, 500)
 
 })
 
+
+//restart button
+
+restart.addEventListener('click', function() {
+
+    // <!-- Logica classi body & tasto restart (colore) -->
+
+    if (countdown.innerText === 'Buon Anno!') {
+        
+        body.classList.toggle('successful');
+        body.classList.add('default-body');
+        
+    } else if (countdown.innerText === 'Countdown Interrotto :(') {
+
+        body.classList.toggle('interrotto');
+        body.classList.add('default-body');
+
+    } 
+    
+    countdown.innerText = 'Il Countdown sta per ricominciare';
+
+    // <!-- add/remove classes (bottoni) -->
+    stopbutton.classList.toggle('hidden');
+    restart.classList.toggle('hidden');
+
+    //riassegno 10 al contatore 
+    count = 10;
+
+    myInterval = setInterval(function(){
+        
+        countdown.innerText = count;
+
+        count--;
+
+        if(count < 0) {
+
+            // <!-- add/remove classes (colore) -->
+
+            body.classList.add('successful');
+            body.classList.toggle('default-body');
+
+            clearInterval(myInterval);
+
+            countdown.innerText = 'Buon Anno!';
+
+            // <!-- add/remove classes (bottoni) -->
+
+            restart.classList.toggle('hidden');
+            stopbutton.classList.toggle('hidden');
+            
+        }
+
+    }, 500)
+})
+
+
+//stop button
+
 stopbutton.addEventListener('click', function() {
 
-    countdown.innerText = 'HAI STOPPATO IL COUNTDOWN'
-    clearInterval(myInterval)
-    count = 10
+// <!-- add/remove classes (colore) -->
+
+    body.classList.toggle('default-body');
+    body.classList.add('interrotto');
+
+    countdown.innerText = 'Countdown Interrotto :(';
+    clearInterval(myInterval);
+
+// <!-- add/remove classes (bottoni) -->
+
+    start.classList.add('hidden');
+    stopbutton.classList.add('hidden');
+    restart.classList.toggle('hidden');
 
 })
     
 
-    
-    
 
